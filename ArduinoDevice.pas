c28449fd6774008d64564569e22c9357ec6f2997
serhiykobyakov <photokobyakov@gmail.com>
2022-09-05 15:21:37 +0200

    new implementation of basic functions
    a lot of changes

--- ArduinoDevice.pas
+++ ArduinoDevice.pas
@@ -1,5 +1,9 @@
 unit ArduinoDevice;
 
+{ Copyright: (c) Serhiy Kobyakov
+
+Version: 05.09.2022 }
+
 
 interface
 
@@ -23,21 +27,38 @@ type
 
     private
       ser: TBlockSerial;
+      function GetComPort: string;
+      function GetComPortSpeed: integer;
+      function GetDeviceID: string;
+      function GetInitTimeout: integer;
+      function GetLongReadTimeout: integer;
+      function GetReadTimeout: integer;
 
     protected
+      theDeviceID: string;
+      theComPort: string;
+      theComPortSpeed: integer;
+      theInitTimeout: integer;
+      theLongReadTimeout: integer;
+      theReadTimeout: integer;
+
+      // send string and get string
       function SendAndGetAnswer(str: string): string;
+
+      // send single character and get string
+      // faster communication when you need to send only a single byte
+      function SendCharAndGetAnswer(ch: char): string;
 
     public
       constructor Init(ComPort: string);
       destructor Done;
 
-    public
-      theDeviceID: string; static;
-      theComPort: string; static;
-      ComPortSpeed: integer; static;
-      InitTimeout: integer; static;
-      LongReadTimeout: integer; static;
-      ReadTimeout: integer; static;
+      property DeviceID: string Read GetDeviceID;
+      property ComPort: string Read GetComPort;
+      property ComPortSpeed: integer Read GetComPortSpeed;
+      property InitTimeout: integer Read GetInitTimeout;
+      property LongReadTimeout: integer Read GetLongReadTimeout;
+      property ReadTimeout: integer Read GetReadTimeout;
   end;
 
 
@@ -46,6 +67,36 @@ implementation
 
 
 { _ArduinoDevice }
+
+function _ArduinoDevice.GetComPort: string;
+begin
+  Result := theComPort;
+end;
+
+function _ArduinoDevice.GetComPortSpeed: integer;
+begin
+  Result := theComPortSpeed;
+end;
+
+function _ArduinoDevice.GetDeviceID: string;
+begin
+  Result := theDeviceID;
+end;
+
+function _ArduinoDevice.GetInitTimeout: integer;
+begin
+  Result := theInitTimeout;
+end;
+
+function _ArduinoDevice.GetLongReadTimeout: integer;
+begin
+  Result := theLongReadTimeout;
+end;
+
+function _ArduinoDevice.GetReadTimeout: integer;
+begin
+  Result := theReadTimeout;
+end;
 
 function _ArduinoDevice.SendAndGetAnswer(str: string): string;
 begin
@@ -62,6 +113,21 @@ begin
     end;
 end;
 
+function _ArduinoDevice.SendCharAndGetAnswer(ch: char): string;
+begin
+  ser.SendByte(ord(ch));
+  ser.Flush;
+  if ser.canread(LongReadTimeout) then
+    Result := ser.Recvstring(ReadTimeout)
+  else
+    begin
+      showmessage(theDeviceID + LineEnding +
+                  'no responce to "' + ch + '" command!' + LineEnding +
+                  'Check connection or device power');
+      Result := '';
+    end;
+end;
+
 
 constructor _ArduinoDevice.Init(ComPort: string);
 var
@@ -71,13 +137,18 @@ begin
 
 {$IFDEF Linux}
 // remove lock-file
-// I assume that the main application (or some other) can cometimes leave lock file
-// so it would be better to remove it before w econnect to the device
+// I assume that the application we use have to be the one which use
+// the serial comunication on this PC
+// remove it if it is not the case but be ready to communication failiure
+// when the lock-file exists
   FindFiles := TStringList.Create;
   try
     FindAllFiles(FindFiles, '/var/lock/', '*' + ExtractFileName(ComPort), true);
     if (FindFiles.Count = 1) then DeleteFile(FindFiles.Strings[0]);
   finally
+    If FindFiles.Count > 0 then
+//    FileExists(FindFiles.Strings[0]) then
+      showmessage('Can''t remove lock-file:' + LineEnding + FindFiles.Strings[0]);
     FindFiles.Free;
   end;
 {$ENDIF}
@@ -85,10 +156,10 @@ begin
   ser := TBlockSerial.Create;
   try
     ser.Connect(ComPort);
-    ser.config(ComPortSpeed, 8, 'N', SB1, False, False);
-    sleepFor(InitTimeout);
+    ser.config(theComPortSpeed, 8, 'N', SB1, False, False);
+    sleepFor(theInitTimeout);
     ser.SendString('?');
-    if ser.canread(5000) then ser.Recvstring(100); // read first answer
+    if ser.canread(500) then ser.Recvstring(100); // read first answer
   finally
     ser.Flush;
   end;
---
+++ NewDevice_template.pas
@@ -0,0 +1,144 @@
+unit NewDevice;
+
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, dialogs, StdCtrls, Controls, Forms,
+  IniFiles,
+//  strutils,
+  addfunc,
+  ArduinoDevice;
+
+
+type
+
+  { NewDevice_device }
+
+  NewDevice_device = Object (_ArduinoDevice)
+    private
+
+    public
+      constructor Init(_ComPort: string);
+      destructor Done;
+
+// example NewDevice functions:
+      procedure Open;
+      procedure Close;
+  end;
+
+
+implementation
+
+
+constructor NewDevice_device.Init(_ComPort: string);
+var
+  MyForm: TForm;
+  MyLabel: TLabel;
+  AppIni: TIniFile;
+  UpperInitStr, iniFile: string;
+begin
+// -----------------------------------------------------------------------------
+// the device ID string with which it responds to '?'
+  theDeviceID := 'NewDevice';
+// -----------------------------------------------------------------------------
+
+  iniFile := Application.Location + theDeviceID + '.ini';
+
+  If not FileExists(iniFile) then
+    begin
+      showmessage('File ' + LineEnding + iniFile + LineEnding +
+                  'has not been found!' + LineEnding + LineEnding +
+                  'Please fix it');
+      halt(0);
+    end;
+
+// make a splash screen
+// which shows initialization process
+  MyForm := TForm.Create(nil);
+  with MyForm do begin
+     SetBounds(0, 0, 450, 90); Position:=poDesktopCenter; BorderStyle := bsNone;
+     MyForm.Color := $00EEEEEE; end;
+
+  MyLabel := TLabel.Create(MyForm);
+  with MyLabel do begin
+     Autosize := True; Align := alNone; Alignment := taCenter; Parent := MyForm;
+     Visible := True; AnchorVerticalCenterTo(MyForm);
+     AnchorHorizontalCenterTo(MyForm); end;
+
+  MyForm.Show; MyForm.BringToFront;
+
+  UpperInitStr := 'Initializing ' + theDeviceID + ':' + LineEnding;
+
+  MyLabel.Caption:= UpperInitStr + 'Reading ' + theDeviceID + '.ini...';
+  sleepFor(50); // refresh the Label to see the change
+
+// -----------------------------------------------------------------------------
+// Read the device variables from ini file:
+  AppIni := TInifile.Create(iniFile);
+  theComPortSpeed := AppIni.ReadInteger(theDeviceID, 'ComPortSpeed', 115200);
+
+// max time in ms the device may take for its internal initialization
+  theInitTimeout := AppIni.ReadInteger(theDeviceID, 'InitTimeout', 3000);
+
+// max time in ms the device may take before answer
+// it is good idea to measure the longest run
+// before assign the value
+  theLongReadTimeout := AppIni.ReadInteger(theDeviceID, 'LongReadTimeout', 3000);
+
+// max time in ms the device may take before answer
+// in the case of simple and fast queries
+  theReadTimeout := AppIni.ReadInteger(theDeviceID, 'ReadTimeout', 1000);
+
+// other device-specific paremeters must be found in the ini-file
+// and we read them here:
+
+
+  AppIni.Free;
+// -----------------------------------------------------------------------------
+
+// Use basic device initialization
+  MyLabel.Caption:= UpperInitStr + 'Connecting to ' + _ComPort + '...';
+  sleepFor(200); // refresh the Label to see the change
+  Inherited Init(_ComPort);
+
+// Put here the commands which have to be executed right after
+// we connect to the device:
+// Set the shutter into the start position as a last step of initialization
+//  MyLabel.Caption:= UpperInitStr + 'Going to starting position...';
+//  sleepFor(50); // small delay to refresh the Label
+//  SendAndGetAnswer('i');
+
+  MyLabel.Caption:= UpperInitStr + 'Done!';
+  sleepFor(500); // refresh the Label just to see "Done"
+  MyForm.Close;
+  FreeAndNil(MyForm);
+end;
+
+destructor NewDevice_device.Done;
+begin
+// Put here the commands which have to be executed before disconnecting the device
+//  SendCharAndGetAnswer('o');
+  Inherited Done;
+end;
+
+procedure NewDevice_device.Open;
+begin
+// I don't use the device answer here to improve reliability
+// but SendAndGetAnswer returns '0' after 'o' if everything is OK
+//    SendCharAndGetAnswer('o');
+end;
+
+procedure NewDevice_device.Close;
+begin
+// I don't use the device answer here to improve reliability
+// but SendAndGetAnswer returns '0' after 'c' if everything is OK
+//    SendCharAndGetAnswer('c');
+end;
+
+
+end.
+
+
