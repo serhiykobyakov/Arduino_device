e8a93527ec9397232f355ae8bee2ee44e4c3c73f
serhiykobyakov <photokobyakov@gmail.com>
2022-10-10 11:07:12 +0200

    code rearrangement

--- Python/arduino_device.py
+++ Python/arduino_device.py
@@ -1,6 +1,6 @@
-"""Base Arduino device class"""
+""" Base Arduino device class """
 
-__version__ = '09.10.2022'
+__version__ = '10.10.2022'
 __author__ = 'Serhiy Kobyakov'
 
 # import time
@@ -11,26 +11,37 @@ import serial.tools.list_ports
 
 class ArduinoDevice:
     """Basic arduino device class"""
-    device_name = "ArduinoDevice"
-    device_info = ""
-    ser = None
-    comport = ""
 
-    COMPORTSPEED = 115200
+    # serial communication constants:
     COMPORTPARITY = serial.PARITY_NONE
     COMPORTSTOPBITS = serial.STOPBITS_ONE
     COMPORTBITS = serial.EIGHTBITS
-    COMPORTTIMEOUT = 0.4
-    COMPORTWRITETIMEOUT = 0.1
-    # the longest time which device may need to finish the task
+    COMPORTSPEED = 115200
+
+    # the longest time which is necessary to send the command to device:
+    COMPORTWRITETIMEOUT = 0.2
+
+    # the shortest time the device may need to finish the task and give an answer
+    COMPORTREADTIMEOUT = 1
+
+    # the longest time the device may need to finish the task and give an answer
     COMPORTLONGREADTIMEOUT = 5
+
+    # the shortest time between consequent device commands
+    # it is not reasonable in this short time to ask the device do something twice
+    # in this case it is better that the device just return previous answer
+    # to the second inquiry:
     SHORTESTTIMEBETWEENREADS = 0.46
 
+    _device_name = "ArduinoDevice"
+    _device_info = ""
+    _ser = None
+
     def __repr__(self) -> str:
-        return f'{self.device_name} at {self.comport}'
+        return f'{self._device_name} at {self._ser.port}'
 
     def __str__(self) -> str:
-        return f'{self.device_info}'
+        return f'{self._device_info}'
 
     @classmethod
     def get_device_id_str(cls, comport) -> str:
@@ -39,85 +50,92 @@ class ArduinoDevice:
         if not isinstance(comport, str):
             raise TypeError(f"comport: string value expected, got {type(str)} instead")
 
-        ser = serial.Serial(port=comport,
+        cls._ser = serial.Serial(port=comport,
                             baudrate=cls.COMPORTSPEED,
                             writeTimeout=cls.COMPORTWRITETIMEOUT,
-                            timeout=cls.COMPORTTIMEOUT,
+                            timeout=cls.COMPORTREADTIMEOUT,
                             parity=cls.COMPORTPARITY,
                             stopbits=cls.COMPORTSTOPBITS,
                             bytesize=cls.COMPORTBITS)
         try:
-            ser.write(b'?')
-            result = ser.readline().strip()
+            cls._ser.write(b'?')
+            result = cls._ser.readline().strip()
             if len(result) == 0:
                 # if the device doesn't respond immediately it may be
                 # a board with non-native USB
-                ser.timeout = 5  # suppose the board has not been initialized yet.
-                # give it 5 seonds to do it, but no more!
-                # If i'ts not an Arduino, this routine will block the main app
+                cls._ser.timeout = 5  # suppose the board has not been initialized yet.
+                # give it 5 seconds to do it, but no more!
+                # If it's not an Arduino, this routine will block the main app
                 # for this amount of time (for each device!)
-                result = ser.readline().strip()
-                # ser.write(b'?')
-                # ser.timeout = cls.COMPORTTIMEOUT
-                # result = ser.readline().strip()
+                result = cls._ser.readline().strip()
+                # cls._ser.write(b'?')
+                # cls._ser.timeout = cls.COMPORTTIMEOUT
+                # result = cls._ser.readline().strip()
         finally:
-            ser.close()
+            cls._ser.close()
         return result.decode()
 
-    # def __init__(self):
-    #     pass
-
-    def start_serial_communication(self, comport):
-        """Device initialization - connecting to comport"""
+    def __init__(self, comport):
+        """Device initialization - connecting to comport, gathering info etc. """
         # getting the device info:
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if port.device == comport:
                 # save all comport parameters to info:
                 list_of_strings = [f'{key}: {port.__dict__[key]}' for key in port.__dict__]
-                self.device_info = "\n".join(list_of_strings)
+                self._device_info = "\n".join(list_of_strings)
 
-        # connecting to comport:
-        self.ser = serial.Serial(port=comport,
-                                   baudrate=self.COMPORTSPEED,
-                                   write_timeout=self.COMPORTWRITETIMEOUT,
-                                   timeout=self.COMPORTTIMEOUT,
-                                   parity=self.COMPORTPARITY,
-                                   stopbits=self.COMPORTSTOPBITS,
-                                   bytesize=self.COMPORTBITS)
-        self.comport = comport
-        self.ser.write(b'?')
-        result = self.ser.readline().strip()
-        if len(result) == 0:
-            self.ser.timeout = 5
-            result = self.ser.readline().strip()
-            self.ser.timeout = self.COMPORTTIMEOUT
-        if self.device_name != result.decode('UTF-8'):
-            print(f"Error: got {result.decode('UTF-8')}, but expected {self.device_name} \
-while establishing serial communication!")
-
-    def read_basic_parameters(self, inifname):
-        """ read the device parameters from INI file """
+        # read the device parameters from INI file
+        inifname = self._device_name + '.INI'
         config = configparser.ConfigParser()
         config.read(inifname)
+        self.COMPORTSPEED = 0
         self.COMPORTSPEED = int(config['serial']['COMPORTSPEED'])
-        self.COMPORTTIMEOUT = float(config['serial']['TIMEOUT'])
+        if self.COMPORTSPEED == 0:
+            print(f"Error reading {inifname} file!")
+        self.COMPORTREADTIMEOUT = float(config['serial']['READTIMEOUT'])
         self.COMPORTWRITETIMEOUT = float(config['serial']['WRITETIMEOUT'])
         self.COMPORTLONGREADTIMEOUT = float(config['serial']['LONGREADTIMEOUT'])
         self.SHORTESTTIMEBETWEENREADS = float(config['serial']['SHORTESTTIMEBETWEENREADS'])
+        # connecting to comport:
+        self._ser = serial.Serial(port=comport,
+                                   baudrate=self.COMPORTSPEED,
+                                   write_timeout=self.COMPORTWRITETIMEOUT,
+                                   timeout=self.COMPORTREADTIMEOUT,
+                                   parity=self.COMPORTPARITY,
+                                   stopbits=self.COMPORTSTOPBITS,
+                                   bytesize=self.COMPORTBITS)
+        if not self._ser.isOpen():
+            print(f"Error establishing communication with {comport} device!")
+        self._ser.write(b'?')
+        result = self._ser.readline().strip()
+        if len(result) == 0:
+            self._ser.timeout = 5
+            result = self._ser.readline().strip()
+            self._ser.timeout = self.COMPORTREADTIMEOUT
+        if self._device_name != result.decode('UTF-8'):
+            print(f"Error: got {result.decode('UTF-8')}, but expected {self._device_name} \
+while establishing _serial communication!")
 
     def __del__(self):
-        self.ser.close()
+        self._ser.close()
+
+    @property
+    def device_info(self) -> str:
+        # getting all the device info into text:
+        list_of_prop = [f'{key}: {self.__dict__[key]}' for key in self.__dict__ if key != "_device_info"]
+        the_info = self._device_info + "\n" + "\n".join(list_of_prop)
+        return the_info
 
     def send_and_get_answer(self, cmd) -> str:
         """ send command and get answer, short timeout """
-        self.ser.write(cmd.encode())
-        return self.ser.readline().strip().decode()
+        self._ser.write(cmd.encode())
+        return self._ser.readline().strip().decode()
 
     def send_and_get_late_answer(self, cmd) -> str:
         """ send command and get answer, long timeout """
-        self.ser.timeout = self.COMPORTLONGREADTIMEOUT
-        self.ser.write(cmd.encode())
-        answer = self.ser.readline().strip().decode()
-        self.ser.timeout = self.COMPORTTIMEOUT
+        self._ser.timeout = self.COMPORTLONGREADTIMEOUT
+        self._ser.write(cmd.encode())
+        answer = self._ser.readline().strip().decode()
+        self._ser.timeout = self.COMPORTREADTIMEOUT
         return answer
--- Python/new_device.py
+++ Python/new_device.py
@@ -1,6 +1,6 @@
 """ New Arduino device class template """
 
-__version__ = '09.10.2022'
+__version__ = '10.10.2022'
 __author__ = 'Serhiy Kobyakov'
 
 from arduino_device import ArduinoDevice
@@ -10,21 +10,24 @@ class NewDevice(ArduinoDevice):
     """ New Arduino device class template """
     # define the device name:
     # this is the string with which the device responds to b'?' query
-    device_name = "NewDevice"
+    _device_name = "NewDevice"
+
+    # other device-specific variables go here:
 
     def __init__(self, comport):
-        super().__init__()
-        self.read_basic_parameters(self.device_name + '.INI')
+        super().__init__(comport)
+        # self.read_basic_parameters(self._device_name + '.INI')
         # read the device parameters from INI file
         # all except COMPORTSPEED, TIMEOUT, WRITETIMEOUT, SHORTESTTIMEBETWEENREADS
         # YOU STILL HAVE TO PUT ALL THE NECESSARY VALUES INTO INI FILE!!!
         # config = configparser.ConfigParser()
-        # config.read(self.__device_name + '.INI')
-        # self.[some parameter] = config[self.__device_name]['some parameter']
+        # config.read(self._device_name + '.INI')
+        # self.[some parameter] = config[self._device_name]['some parameter']
 
-        self.start_serial_communication(comport)
+        # do some default device-specific init actions here:
 
     def __del__(self):
-        # some stuff before closing connection to the device
-        super().__del__()
+        # do some default device-specific finalization actions here:
 
+        # it must be here to close the serial communication correctly:
+        super().__del__()
