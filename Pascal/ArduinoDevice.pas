a1f1519a138725fc7ccec95dd50670f6f603970c
Serhiy Kobyakov <photokobyakov@gmail.com>
2023-10-03 19:23:07 +0200

    stability improvements

--- Pascal/ArduinoDevice.pas
+++ Pascal/ArduinoDevice.pas
@@ -1,7 +1,7 @@
 unit ArduinoDevice;
 {
 Base Arduino device unit
-Version: 11.09.2022
+Version: 2023.10.03
 
 (c) Serhiy Kobyakov
 }
@@ -34,6 +34,8 @@ type
       function GetLongReadTimeout: integer;
       function GetReadTimeout: integer;
 
+      procedure WriteToLog(str: string);
+
     protected
       theDeviceID: string;
       theComPort: string;
@@ -43,7 +45,9 @@ type
       theReadTimeout: integer;
 
 // send string and get string
-      function SendAndGetAnswer(str: string): string;
+      function SendStr(cmd: string): boolean;
+      function ReceiveStr(): string;
+      function SendAndGetAnswer(cmd: string): string;
 
 // send single character and get string
 // faster communication when you need to send a single byte only
@@ -98,24 +102,113 @@ begin
   Result := theReadTimeout;
 end;
 
-function _ArduinoDevice.SendAndGetAnswer(str: string): string;
+procedure _ArduinoDevice.WriteToLog(str: string);
+// write a log message to file
+var
+  fileName: string;
+  f: Text;
 begin
-  ser.SendString(str);
-//  ser.Flush;
-  if ser.canread(LongReadTimeout) then
-    Result := ser.Recvstring(ReadTimeout)
-  else
-    begin
-      showmessage(theDeviceID + LineEnding +
-                  'no responce to "' + str + '" command!' + LineEnding +
-                  'Check connection or device power');
-      Result := '';
+  fileName := theDeviceID + '.log';
+  Assign(f, fileName);
+
+  if not FileExists(fileName) then Rewrite(f);
+
+  Append(f);
+  WriteLn(f, DateTimeToStr(Now) + ' ' + theDeviceID + ' at ' + theComPort);
+  WriteLn(f, str);
+  WriteLn(f, LineEnding);
+  Close(f);
+end;
+
+function _ArduinoDevice.SendStr(cmd: string): boolean;
+var
+  sendOK: boolean;
+  attempt_counter, bytesSent: Word;
+begin
+  sendOK := False;
+  attempt_counter := 0;
+
+  Repeat
+    attempt_counter := attempt_counter + 1;
+
+    try
+      bytesSent := 0;
+      bytesSent := ser.SendBuffer(Pointer(cmd), Length(cmd));
+    finally
     end;
+
+    if (bytesSent = Length(cmd)) then sendOK := True;
+
+    if (not sendOK) then
+      begin
+        WriteToLog('  problem sending ' + cmd + ' command' + LineEnding +
+                   '  ' + IntToStr(bytesSent) + ' bytes sent instead of ' +
+                   IntToStr(Length(cmd)) + LineEnding +
+                   '  this is ' + IntToStr(attempt_counter) + ' attempt');
+        sleepFor(Round(theLongReadTimeout/20));
+      end;
+
+    if attempt_counter > 5 then Break;
+
+  Until sendOK;
+
+  Result := sendOK;
+end;
+
+function _ArduinoDevice.ReceiveStr: string;
+// base function to receive string from device
+// use it in your device unit!
+var
+  readOK: boolean;
+  attempt_counter: Word;
+  theResult: string;
+  startReading: TDateTime;
+begin
+  readOK := False;
+  attempt_counter := 0;
+
+  Repeat
+    attempt_counter := attempt_counter + 1;
+    startReading := Now;
+    Repeat
+      try
+        theResult := '';
+        theResult := ser.Recvstring(theLongReadTimeout)
+      finally
+        if length(theResult) = 0 then sleepFor(20);
+      end;
+
+      // escape the loop if we spent too much time inside the loop
+      if MillisecondsBetween(Now, startReading) > theLongReadTimeout then Break;
+
+    Until length(theResult) > 0;
+
+    if (length(theResult) > 0) then readOK := True
+    else WriteToLog('  problem reading from serial port' + LineEnding +
+                    '  this is ' + IntToStr(attempt_counter) + ' attempt' + LineEnding +
+                    '  the result: ' + theResult + ' len: ' + IntToStr(length(theResult)));
+
+    // escape the function if we fail 6 times
+    if attempt_counter > 5 then Break;
+
+  Until readOK;
+
+  Result := theResult;
+end;
+
+
+function _ArduinoDevice.SendAndGetAnswer(cmd: string): string;
+begin
+  SendStr(cmd);
+  sleepFor(20); // there must be some time between sending and receiving!
+  Result := ReceiveStr();
 end;
 
 function _ArduinoDevice.SendCharAndGetAnswer(ch: char): string;
 begin
+// send command
   ser.SendByte(ord(ch));
+
 //  ser.Flush;
   if ser.canread(LongReadTimeout) then
     Result := ser.Recvstring(ReadTimeout)
