f47c0eb7eec35bbfd356be351ffbe3127acdeb70
serhiykobyakov <photokobyakov@gmail.com>
2025-02-09 17:00:36 -0800

    formatting

--- Python/arduino_device.py
+++ Python/arduino_device.py
@@ -1,8 +1,8 @@
 """class ArduinoDevice is a base class
 for devices built on Arduino boards using serial communication"""
 
-__version__ = '29.03.2024'
-__author__ = 'Serhiy Kobyakov'
+__version__ = "29.03.2024"
+__author__ = "Serhiy Kobyakov"
 
 import time
 import os
@@ -37,10 +37,10 @@ class ArduinoDevice:
     _lastcomtimestamp = 0.0
 
     def __repr__(self) -> str:
-        return f'{self._device_name} at {self._ser.port}'
+        return f"{self._device_name} at {self._ser.port}"
 
     def __str__(self) -> str:
-        return f'{self._device_name} at {self._ser.port}'
+        return f"{self._device_name} at {self._ser.port}"
 
     @classmethod
     def get_arduino_serial_devices_dict(cls):
@@ -69,8 +69,7 @@ class ArduinoDevice:
         result = b""
         if not isinstance(comport, str):
             raise TypeError(
-                "comport: string value expected, "
-                f"got {type(str)} instead"
+                "comport: string value expected, " f"got {type(str)} instead"
             )
         # if the device doesn't respond immediately it may be
         # a board with non-native USB
@@ -82,11 +81,11 @@ class ArduinoDevice:
         cls._ser = serial.Serial(
             port=comport,
             baudrate=cls.COMPORTSPEED,
-            writeTimeout=2.,
-            timeout=5.,
+            writeTimeout=2.0,
+            timeout=5.0,
             parity=cls.COMPORTPARITY,
             stopbits=cls.COMPORTSTOPBITS,
-            bytesize=cls.COMPORTBITS
+            bytesize=cls.COMPORTBITS,
         )
         try:
             cls._ser.flush()
@@ -110,7 +109,7 @@ class ArduinoDevice:
         instance = super().__new__(cls)
 
         # check if ini file exists
-        if not os.path.isfile(cls._device_name + '.INI'):
+        if not os.path.isfile(cls._device_name + ".INI"):
             print(f"\n  ERROR: no {cls._device_name + '.INI'} file found!\n")
             return
 
@@ -122,23 +121,20 @@ class ArduinoDevice:
         """
 
         # read the serial port parameters from INI file
-        inifname = self._device_name + '.INI'
+        inifname = self._device_name + ".INI"
         with open(inifname, "r") as f:
             config = configparser.ConfigParser()
             config.read_file(f)
             self.COMPORTREADTIMEOUT = -1
-            self.COMPORTREADTIMEOUT = \
-                float(config['serial']['READTIMEOUT'])
+            self.COMPORTREADTIMEOUT = float(config["serial"]["READTIMEOUT"])
             if self.COMPORTREADTIMEOUT == -1:
                 print(
                     f"  {self._device_name} at {comport}: "
                     f"Error reading {inifname} file!"
                 )
                 return
-            self.COMPORTWRITETIMEOUT = \
-                float(config['serial']['WRITETIMEOUT'])
-            self.COMPORTLONGREADTIMEOUT = \
-                float(config['serial']['LONGREADTIMEOUT'])
+            self.COMPORTWRITETIMEOUT = float(config["serial"]["WRITETIMEOUT"])
+            self.COMPORTLONGREADTIMEOUT = float(config["serial"]["LONGREADTIMEOUT"])
 
         # connecting to comport:
         self._ser = serial.Serial(
@@ -148,17 +144,17 @@ class ArduinoDevice:
             timeout=self.COMPORTREADTIMEOUT,
             parity=self.COMPORTPARITY,
             stopbits=self.COMPORTSTOPBITS,
-            bytesize=self.COMPORTBITS
+            bytesize=self.COMPORTBITS,
         )
         if not self._ser.isOpen():
             print(f"Error establishing communication with {comport} device!")
-        self._ser.write(b'?')
+        self._ser.write(b"?")
         result = self._ser.readline().strip()
         if len(result) == 0:
             self._ser.timeout = 5
             result = self._ser.readline().strip()
             self._ser.timeout = self.COMPORTREADTIMEOUT
-        if self._device_name != result.decode('UTF-8'):
+        if self._device_name != result.decode("UTF-8"):
             print(
                 f"Error: got {result.decode('UTF-8')}, "
                 f"but expected {self._device_name} "
@@ -181,9 +177,7 @@ class ArduinoDevice:
         """
         get device-specific attributes as a text
         """
-        list_of_prop = [
-            f'{key}: {self.__dict__[key]}' for key in self.__dict__
-        ]
+        list_of_prop = [f"{key}: {self.__dict__[key]}" for key in self.__dict__]
         the_info = "\n".join(list_of_prop)
         return the_info
 
@@ -193,7 +187,7 @@ class ArduinoDevice:
         get serial communication attributes as a text
         """
         list_of_strings = [
-            f'{key}: {self._ser.__dict__[key]}' for key in self._ser.__dict__
+            f"{key}: {self._ser.__dict__[key]}" for key in self._ser.__dict__
         ]
         the_info = "\n".join(list_of_strings)
         return the_info
@@ -201,14 +195,14 @@ class ArduinoDevice:
     @property
     def comport_info(self) -> str:
         """
-        get COM port attributes as a text 
+        get COM port attributes as a text
         """
         list_of_strings = []
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if port.device == self._ser.name:
                 list_of_strings = [
-                    f'{key}: {port.__dict__[key]}' for key in port.__dict__
+                    f"{key}: {port.__dict__[key]}" for key in port.__dict__
                 ]
         the_info = "\n".join(list_of_strings)
         return the_info
